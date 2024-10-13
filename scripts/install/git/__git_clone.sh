e031882b0546d54ef2791aa4c57386c1994d4f25
sm41 <39440128+sm41@users.noreply.github.com>
2024-10-14 08:26:04 +0900

    fix

--- scripts/install/git/__git_clone.sh
+++ scripts/install/git/__git_clone.sh
@@ -1,19 +1,26 @@
 # !/bin/bash
-set -eux
+# set -eux
 
 input_file=git_repo.txt
 work_path=$(realpath $(dirname "$0"))
 
-cd ${HOME}/repository
 
+source ${work_path}/${input_file}
+
+# cd ${HOME}/repository
 
 while read repository_url
 do
 
-  [ -z ${repository_url} ] && continue
-  [ ${repository_url::1} = "#" ] && continue
+  [[ -z "${repository_url}" ]] && continue
+  [[ "${repository_url::1}" = "#" ]] && continue
+  [[ ! "${repository_url}" =~ ^declare ]] && continue
 
-  git clone ${repository_url}.git
+  var_name=$(echo "${repository_url}" |grep -oP '(?<=declare -A ).*(?==\()')
+  declare -n var_array=${var_name}
+
+  echo ${var_array[title]}
+  # git clone ${repository_url}.git
 
 done < ${work_path}/${input_file}
 
